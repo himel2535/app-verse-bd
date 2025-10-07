54e26a9030287e8530b5d55da1710ce4cbe1c75f
himel2535 <monwarhossanhimel@gmail.com>
2025-10-07 23:35:15 +0600

    vite,tailwind,daisyUI and react router set up

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>kocormocor</div>,
+  },
+]);
